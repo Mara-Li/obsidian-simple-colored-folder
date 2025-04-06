08d536d3f6d24051aeda8521a642aab752225f20
Mara <mara-li@outlook.fr>
2025-04-06 10:58:13 +0200

    fix: warning at each obsidian start because style settings is debounced

--- src/main.ts
+++ src/main.ts
@@ -94,8 +94,7 @@ export default class SimpleColoredFolder extends Plugin {
 			returnEmptyString: false,
 		});
 
-		const styleSettings = this.app.plugins.getPlugin("obsidian-style-settings");
-		if (!styleSettings) {
+		if (!this.app.plugins.enabledPlugins.has("obsidian-style-settings")) {
 			new Notice(
 				sanitizeHTMLToDom(
 					dedent`<span class="spf-warning">${i18next.t("warning")}</span>`
